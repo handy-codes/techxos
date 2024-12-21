d50ca700530339ca9df283ef10596bf14ba4eab8
emeka <emekaowo@yahoo.com>
2024-12-21 14:36:13 +0100

    fix

--- components/layout/Intro.tsx
+++ components/layout/Intro.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Intro() {
   return (
     <div className="p-3 sm:p-8">
-      <div className="bg-[#1A2E05] border border-[#BBE98A] borders text-[#bbe98a] h-[40vh] p-4 sm:p-8 gap-2 sm:gap-2 radius flex flex-col items-center">
+      <div className="bg-[#1A2E05] border border-[#ECEFF1] borders text-[#bbe98a] h-[40vh] p-4 sm:p-8 gap-2 sm:gap-2 radius flex flex-col items-center">
         <h1 className='sm:font-bold text-[28px] sm:text-[34px] md:text-5xl'>Our Course Gallery</h1>
         <p className='text-white text-justify text-[14px] max-sm:hidden md:px-44 md:text-[17px]'>
           Techxos is a dedicated online learning App. Made of local content, you can choose from our below well-curated 
