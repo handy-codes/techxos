3ed7defcddeb75350b846bb6a7c9c6582778b954
emeka <emekaowo@yahoo.com>
2024-12-21 14:28:12 +0100

    fix

--- components/layout/Welcome.tsx
+++ components/layout/Welcome.tsx
@@ -1,14 +1,14 @@
 'use client';
 
-import { useUser } from '@clerk/nextjs';
 import React from 'react'
+import { useUser } from '@clerk/nextjs';
 
 const Welcome = () => {
   const {user} = useUser();
 
   return (
     user && (
-    <div className='px-8 pt-4 flex flex-wrap gap-3'><h1 className='text-[#ECEFF1] text-3xl'>Welcome</h1> <span className='text-[#03FF01] text-4xl'>{user?.firstName}</span></div>
+    <div className='px-8 pt-4 flex items-center flex-wrap gap-3'><h1 className='text-[#ECEFF1] text-3xl'>Welcome</h1> <span className='text-[#03FF01] text-4xl'>{user?.firstName}</span></div>
    )
   )
 }
