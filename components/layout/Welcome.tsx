efd3afabd67b49840d7c33b9c6c4e948808fb348
emeka <emekaowo@yahoo.com>
2024-12-21 14:29:37 +0100

    fix

--- components/layout/Welcome.tsx
+++ components/layout/Welcome.tsx
@@ -8,7 +8,7 @@ const Welcome = () => {
 
   return (
     user && (
-    <div className='px-8 pt-4 flex items-center flex-wrap gap-3'><h1 className='text-[#ECEFF1] text-3xl'>Welcome</h1> <span className='text-[#03FF01] text-4xl'>{user?.firstName}</span></div>
+    <div className='px-8 pt-4 flex items-center flex-wrap gap-2'><h1 className='text-[#ECEFF1] text-3xl'>Welcome</h1> <span className='text-[#03FF01] text-4xl'>{user?.firstName}</span></div>
    )
   )
 }
