af6495d7fa529dd40dff1c573e54faf59caf76ab
emeka <emekaowo@yahoo.com>
2024-12-24 21:37:56 +0100

    fix

--- components/layout/Intro.tsx
+++ components/layout/Intro.tsx
@@ -4,13 +4,13 @@ import React from 'react'
 export default function Intro() {
   return (
     <div className="p-3 sm:p-8">
-      <div className="bg-[#1A2E05] border border-[#ECEFF1] borders text-[#03FF01] h-[40vh] p-4 sm:p-8 gap-2 sm:gap-2 radius flex flex-col items-center">
-        <h1 className='sm:font-bold text-[28px] sm:text-[34px] md:text-5xl'>Our Course Gallery</h1>
+      <div className="bg-[#1A2E05] border border-[#ECEFF1] borders text-[#5bd55b] h-[40vh] p-4 sm:p-8 gap-2 sm:gap-2 radius flex flex-col items-center">
+        <h1 className='sm:font-bold text-[28px] sm:text-[34px] md:text-5xl'>Explore Our Courses</h1>
         <p className='text-white text-justify text-[14px] max-sm:hidden md:px-44 md:text-[17px]'>
           Techxos is a dedicated online learning App. Made of local content, you can choose from our below well-curated 
           courses ranging from Coding to Accounting, Tourism & Aviation,
           as well as preparatory courses for ICAN, UTME, SSCE exams and so much more.<br/><br/>
-          Soon, you can upload and earn from your own quality courses.
+          Soon, we will announce when you can as well upload and earn from your own quality courses.
           {/* To upload and earn from your own quality course, please contact <span className='text-[#E79D09]'>sales@techxos.com.</span> */}
         </p>
 
@@ -18,7 +18,7 @@ export default function Intro() {
           Expore our well-curated 
           courses - from Coding to Accounting, Tourism & Aviation,
           as well as preparatory courses for ICAN, UTME, SSCE exams and so much more.<br/><br/>
-          Soon, you can upload and earn from your own quality courses.
+          Soon, we will announce when you can as well upload and earn from your own quality courses.
           {/* To upload and earn from your own quality course, please contact <span className='text-[#E79D09]'>sales@techxos.com.</span> */}
         </p>
       </div>
