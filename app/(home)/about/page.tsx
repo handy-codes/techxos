581463716c349607c6d3e6377e729e726467cc73
emeka <emekaowo@yahoo.com>
2024-12-30 15:25:38 +0100

    fix

--- app/(home)/about/page.tsx
+++ app/(home)/about/page.tsx
@@ -7,17 +7,18 @@ const page = () => {
   return (
   <>
   <div className=''>
+      <h1 className="ml-[5%] sm:ml-14 my-auto gap-2 mx-auto mt-12 text-3xl sm:text-4xl font-bold">Collaborating to <span className='text-[#003E8F] text-4xl sm:text-4xl font-extrabold'>Redefine</span> <br /> Learning</h1>
     <div className="flex-row-reverse my-12  flex flex-wrap max-sm:gap-4 justify-center max-w-7xl mx-auto">
     <div className="radius content-center bg-[#003B65] p-6 w-[90vw] sm:w-[45vw] md:h-[75vh] h-fit flex sm:content-center flex-col sm:items-center sm:justify-center">
-    <h1 className='text-white text-[21px] font-medium sm:text-3xl'>Redefining Learning</h1>
+        <h1 className='text-white text-[21px] font-medium sm:text-3xl'>Scale with Us</h1>
         <p className='text-[#ABBECC] sm:text-justify pt-3 pb-3 text-[18px]'> 
           Techxos is a dedicated online learning App. Made of local content, you can access well-curated courses ranging from Coding to Accounting, Tourism & Aviation,
           as well as in preparatory courses for ICAN, UTME, SSCE exams and so much more. Learn at your own pace.
         </p>
       </div> 
-      <div className=" max-sm:rounded-lg sm:inline-block w-[90vw] sm:w-[45vw] h-[75vh] ">
-        <Image className='radius2 max-sm:border-4 max-sm:border-[#1a2e05] w-[100%] h-[85%] overflow-hidden object-cover' src={'/wandyboss2.jpg'} width={700} height={600} alt=''/>
-        <div className="w-[100%] p-6 h-[15%] flex flex-col justify-center items-center bg-lime-950 py-4 text-white">
+      <div className="radius2 max-sm:rounded-lg sm:inline-block w-[90vw] sm:w-[45vw] h-[75vh] ">
+        <Image className='radius4 max-sm:border-2 max-sm:border-[#1a2e05] w-[100%] h-[85%] overflow-hidden object-cover' src={'/wandyboss2.jpg'} width={700} height={600} alt=''/>
+        <div className=" w-[100%] p-6 h-[15%] flex flex-col justify-center items-center bg-lime-950 py-4 text-white">
          <p className='text-xl'>Jonadab Omonigho</p>
          <p>Founder, CEO Techxos</p>
          {/* <p>HighKlass Intl Schools </p> */}
@@ -28,15 +29,15 @@ const page = () => {
 
     {/* CTO SECTION */}
     <div className=" my-12 flex flex-wrap max-sm:gap-4 justify-center max-w-7xl mx-auto">
-      <div className="radius content-center bg-[#003B65] p-6 w-[90vw] sm:w-[45vw] md:h-[75vh] h-fit flex sm:content-center flex-col sm:items-center sm:justify-center">
-          <h1 className='text-white text-[21px] font-medium sm:text-3xl'>Leaverage Tech | eLearn now!</h1>
+      <div className="radius3 content-center bg-[#003B65] p-6 w-[90vw] sm:w-[45vw] md:h-[75vh] h-fit flex sm:content-center flex-col sm:items-center sm:justify-center">
+          <h1 className='text-white text-[21px] font-medium sm:text-3xl'>Leaverage Tech | e-Learn Today</h1>
           <p className='text-[#ABBECC] sm:text-justify pt-3 pb-3 text-[18px]'> 
             When traditional 9-5pm work schedule leaves you with little time and energy to develop yourself
             further or to read and pass your college or professional exams, Techxos has got you covered. Your instructors never sleep!
           </p>
       </div> 
-      <div className="max-sm:rounded-lg sm:inline-block w-[90vw] sm:w-[45vw] h-[75vh] ">
-        <Image className='radius2 max-sm:border-4 max-sm:border-[#1a2e05] w-[100%] h-[85%] overflow-hidden object-cover' src={'/owo.jpg'} width={700} height={600} alt=''/>
+      <div className=" max-sm:rounded-lg sm:inline-block w-[90vw] sm:w-[45vw] h-[75vh] ">
+        <Image className='radius5 max-sm:border-2 max-sm:border-[#1a2e05] w-[100%] h-[85%] overflow-hidden object-cover' src={'/owo.jpg'} width={700} height={600} alt=''/>
         <div className="w-[100%] p-6 h-[15%] flex flex-col justify-center items-center bg-lime-950 py-4 text-white">
          <p className='text-xl'>Emeka Owo</p>
          <p>CTO & Cofounder, at Techxos</p>
