5255ca4560bc218ee350161a2d49d2945746a4ad
emeka <emekaowo@yahoo.com>
2025-01-02 23:37:23 +0100

    fix

--- components/layout/Welcome.tsx
+++ components/layout/Welcome.tsx
@@ -15,10 +15,21 @@ const Welcome = () => {
     return '';
   };
 
+  const getGreeting = () => {
+    const currentHour = new Date().getHours();
+    if (currentHour < 12) {
+      return 'Good morning';
+    } else if (currentHour < 18) {
+      return 'Good afternoon';
+    } else {
+      return 'Good evening';
+    }
+  };
+
   return (
     user && (
-      <div className='pl-2 pt-4 flex items-center flex-wrap gap-2 mb-3'>
-        <h1 className='text-[#ECEFF1] text-xl sm:text-3xl'>Welcome,</h1>
+      <div className='pl-2 sm:pl-7 pt-4 flex items-center flex-wrap gap-2 mb-3'>
+        <h1 className='text-[#ECEFF1] text-xl sm:text-3xl'>{getGreeting()},</h1>
         <span className='text-[#03FF01] text-2xl sm:text-4xl'>
           {getDisplayName()}
         </span>
