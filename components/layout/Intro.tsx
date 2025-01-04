c6d65cd90651dc1f3e7be189ae22b84a938b6880
emeka <emekaowo@yahoo.com>
2025-01-04 04:53:32 +0100

    fix

--- components/layout/Intro.tsx
+++ components/layout/Intro.tsx
@@ -4,8 +4,7 @@ import React from 'react'
 export default function Intro() {
   return (
     <div className="px-3 py-4 sm:p-8">
-      <div className="bg-[#FF6E01] text-[black] h-fit border border-[#a8e763] borders p-4 sm:p-8 gap-2 sm:gap-2 radius flex flex-col items-center">
-      {/* <div className="bg-[#E79D09] text-[black] h-fit border border-[#a8e763] borders p-4 sm:p-8 gap-2 sm:gap-2 radius flex flex-col items-center"> */}
+      <div className="bg-[#E79D09] text-[black] h-fit border border-[#a8e763] borders p-4 sm:p-8 gap-2 sm:gap-2 radius flex flex-col items-center">
       {/* <div className="bg-[#010101] border border-[#ECEFF1] borders text-[#E79D09] h-[40vh] p-4 sm:p-8 gap-2 sm:gap-2 radius flex flex-col items-center"> */}
         <h1 className='font-bold text-[28px] text-[black] sm:text-[34px] md:text-5xl'>Explore Our Courses</h1>
         <p className='text-[black] font-semibold text-justify text-[14px] max-sm:hidden md:px-44 md:text-[17px]'>
