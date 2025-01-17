bd9c667551a2c58cdf6260897bac0025e9eb988f
emeka <emekaowo@yahoo.com>
2025-01-17 22:24:00 +0100

    fix

--- components/layout/Welcome.tsx
+++ components/layout/Welcome.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-// import React, { useState, useEffect } from "react";
-// import Typical from "react-typical";
 import { useUser } from "@clerk/nextjs";
 import { Typewriter } from 'react-simple-typewriter';
 
@@ -29,41 +27,30 @@ const Welcome = () => {
     }
   };
 
-
   const showTypical = true;
-
-  // const [showTypical, setShowTypical] = useState(false);
-
-  // useEffect(() => {
-  //   const timer = setTimeout(() => {
-  //     setShowTypical(true);
-  //   }, 2000); // 2000 milliseconds (2 seconds) delay
-
-  //   return () => clearTimeout(timer);
-  // }, []);
 
   return (
     user && (
-      <div className="flex flex-wrap sm:gap-2 items-center justify-start">
+      <div className="flex flex-wrap px-3 sm:gap-2 items-center justify-start">
         <div className="px-2 sm:pl-7 pt-4 flex items-center justify-start flex-wrap gap-2 mb-3 welcome-shadow">
-          <h1 className="text-[white] text-3xl sm:text-3xl">
+          <h1 className="text-[white] font-bold text-3xl sm:text-3xl">
             {getGreeting()},
           </h1>
-          <span className="text-[#03FF01] text-3xl sm:text-3xl">
+          <span className="text-[#03FF01] font-bold text-3xl sm:text-3xl">
             {getDisplayName()}
           </span>
         </div>
         <div className="px-2 sm:pl-7 pt-2 sm:pt-4 flex items-center flex-wrap gap-2 mb-3 welcome-shadow">
           {showTypical && (
-            <div className="text-[#D9DD03] font-bold text-[22px] sm:text-3xl">
+            <div className="text-[#D9DD03] font-bold text-3xl">
               <Typewriter
-                words={["Welcome back to the platform!"]}
+                words={["Welcome back to the platform!", "What would you like to learn today?"]}
                 loop={1}
                 cursor
                 cursorStyle="|"
-                typeSpeed={70}
+                typeSpeed={80}
                 deleteSpeed={50}
-                delaySpeed={2000}
+                delaySpeed={3000}
               />
             </div>
           )}
