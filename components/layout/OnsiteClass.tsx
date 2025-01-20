9ff98a100c0879592579b1a7f361185f38cd0d37
emeka <emekaowo@yahoo.com>
2025-01-20 01:45:04 +0100

    fix

--- components/layout/OnsiteClass.tsx
+++ components/layout/OnsiteClass.tsx
@@ -6,42 +6,40 @@ import { HiLocationMarker } from "react-icons/hi";
 import { IoMdOptions } from "react-icons/io";
 
 const OnsiteClass = () => {
-  return (   
+  return (
     <div className="p-4  md:p-10 ">
       <div className="bg-white md:p-4 lg:p-10 rounded-lg shadow-lg">
         <div className="flex flex-col gap-4 md:flex-row h-[80vh]  mx-auto">
           <div className="w-[90vw] py-24 md:py-0 md:w-[55vw] h-[80vh] md:h-[80vh] text-[black] max-md:p-3 p-2 md:p-6">
-            <h1 className="text-2xl lg:text-4xl mt-4 md:mt-0 font-bold mb-4 md:mb-2 lg:mb-6">
-              FRONTEND DEVELOPMENT COURSE
-            </h1>
+            <div className="mt-4 md:mt-0 mb-4 md:mb-2 lg:mb-6">
+              <h1 className="text-2xl lg:text-4xl font-bold mb-[4px]">
+                FRONTEND DEVELOPMENT COURSE
+              </h1>
+              <div className="h-[10px] w-[80px] md:w-[150px] bg-[#E79D09]"></div>
+            </div>
             <h1 className="text-3xl text-[green] lg:text-4xl font-extrabold mb-4 md:mb-2 lg:mb-6">
               150,000 NGN
             </h1>
             <p className="text-justify font-semibold">
-              In 12 weeks, be absorbed in our extensive Frontend Web Development course.
-              Master the art of using ReactJs, TailwindCSS and the Color Theory, to create Websites people will love. 
+              In 12 weeks, be absorbed in our extensive Frontend Web Development
+              course. Master the art of using ReactJs, TailwindCSS and the Color
+              Theory, to create Websites people will love.
             </p>
             <div className="font-semibold">
               <div className="flex items-center gap-2 mt-4 md:mt-4">
-                <FaRegClock className="text-[black] font-bold text-[20px]"/>
-                <span>
-                  Duration: 12 weeks
-                </span>
+                <FaRegClock className="text-[black] font-bold text-[20px]" />
+                <span>Duration: 12 weeks</span>
               </div>
               <div className="flex items-center gap-2 mt-4 md:mt-4">
-                <AiFillSchedule className="text-[black] font-bold text-[20px]"/>
-                <span>
-                  Schedule: 9 hours/week
-                </span>
+                <AiFillSchedule className="text-[black] font-bold text-[20px]" />
+                <span>Schedule: 9 hours/week</span>
               </div>
               <div className="flex items-center gap-2 mt-4 md:mt-4">
-                <HiLocationMarker className="text-[black] font-bold text-[20px]"/>
-                <span>
-                  Location: In-person or online
-                </span>
+                <HiLocationMarker className="text-[black] font-bold text-[20px]" />
+                <span>Location: In-person or online</span>
               </div>
               <div className="flex items-center gap-2 mt-4 md:mt-4">
-                <IoMdOptions className="text-[black] font-bold text-[20px]"/>
+                <IoMdOptions className="text-[black] font-bold text-[20px]" />
                 <span>
                   Options: Evening Class, Executive (one-to-one) class
                 </span>
