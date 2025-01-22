1a2a8772aa53c476abf73e6bb479f9f67e0a7915
emeka <emekaowo@yahoo.com>
2025-01-22 11:25:15 +0100

    fix

--- components/layout/OnsiteClass.tsx
+++ components/layout/OnsiteClass.tsx
@@ -25,23 +25,23 @@ const OnsiteClass = () => {
               course. Master the art of using ReactJs, TailwindCSS and the Color
               Theory, to create Websites people will love.
             </p>
-            <div className="p-4 mt-3 mb-2 hover:bg-green-700 hover:text-white transition border rounded-md border-green-700 inline-block bg-white font-bold">
+            <div className="p-2 md:p-4 my-0 md:mt-3 md:mb-2 hover:bg-green-700 hover:text-white transition border rounded-md border-green-700 inline-block bg-white font-bold">
               <a href="https://wa.me/2349038984567" target="_blank" rel="noopener noreferrer">Contact an Advisor</a>
             </div>
             <div className="font-semibold">
-              <div className="flex items-center gap-3 mt-4 md:mt-4">
+              <div className="flex items-center gap-3 mt-3 md:mt-4">
                 <FaRegClock className="text-[black] font-bold text-[22px]" />
                 <span>Duration: 12 weeks</span>
               </div>
-              <div className="flex items-center gap-3 mt-4 md:mt-4">
+              <div className="flex items-center gap-3 mt-3 md:mt-4">
                 <AiFillSchedule className="text-[black] font-bold text-[24px]" />
                 <span>Schedule: 9 hours/week</span>
               </div>
-              <div className="flex items-center gap-3 mt-4 md:mt-4">
+              <div className="flex items-center gap-3 mt-3 md:mt-4">
                 <HiLocationMarker className="text-[black] font-bold text-[27px]" />
                 <span>Location: In-person or online</span>
               </div>
-              <div className="flex items-center gap-3 mt-4 md:mt-4">
+              <div className="flex items-center gap-3 mt-3 md:mt-4">
                 <IoMdOptions className="text-[black] font-bold text-[24px]" />
                 <span>
                   Options: Evening Class, Executive (one-to-one) class
