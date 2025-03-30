be2e7fe8bd1ea5712d7f44c35918919ed900ae53
emeka <emekaowo@yahoo.com>
2025-03-30 23:22:49 +0100

    fex

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -21,17 +21,17 @@ export const POST = async (req: NextRequest) => {
   }
 
   const session = event.data.object as Stripe.Checkout.Session;
-  const customerId = session?.metadata?.customerId;
+  const userId = session?.metadata?.userId;
   const courseId = session?.metadata?.courseId;
 
   if (event.type === "checkout.session.completed") {
-    if (!customerId || !courseId) {
+    if (!userId || !courseId) {
       return new NextResponse("Missing metadata", { status: 400 });
     }
 
     await db.purchase.create({
       data: {
-        customerId,
+        userId,
         courseId,
       },
     });
