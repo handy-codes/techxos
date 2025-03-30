b4ecd77b1514b6ec1b692eeead5ccccaa48da682
emeka <emekaowo@yahoo.com>
2025-03-30 22:49:20 +0100

    fix

--- (course)/courses/[courseId]/sections/[sectionId]/page.tsx
+++ (course)/courses/[courseId]/sections/[sectionId]/page.tsx
@@ -1,10 +1,26 @@
-export {};
+import { db } from "@/lib/db";
 
-const purchase = await db.purchase.findUnique({
-  where: {
-    userId_courseId: {
-      userId: userId,
-      courseId: params.courseId
+async function CourseSectionPage({
+  params
+}: {
+  params: { courseId: string; sectionId: string }
+}) {
+  const purchase = await db.purchase.findUnique({
+    where: {
+      userId_courseId: {
+        userId: userId,
+        courseId: params.courseId
+      }
     }
-  }
-}); 
+  });
+
+  // ... rest of your code
+
+  return (
+    <div>
+      {/* Your JSX here */}
+    </div>
+  );
+}
+
+export default CourseSectionPage; 
