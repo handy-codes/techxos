d8ecab77856de9d3f61a735fd6d7d0dcf8662e12
emeka <emekaowo@yahoo.com>
2025-03-30 23:19:41 +0100

    fix

--- app/api/courses/[courseId]/checkout/route.ts
+++ app/api/courses/[courseId]/checkout/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { currentUser } from "@clerk/nextjs/server";
-import { auth } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 
 import { db } from "@/lib/db";
 
