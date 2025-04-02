503c421f9114fc036637364e17d2fdcf5628b3e0
emeka <emekaowo@yahoo.com>
2025-04-02 12:05:34 +0100

    fix

--- app/(home)/search/page.tsx
+++ app/(home)/search/page.tsx
@@ -1,5 +1,13 @@
 import CourseCard from "@/components/courses/CourseCard";
 import { db } from "@/lib/db"
+import { Course, Category, SubCategory, Section, Level } from "@prisma/client"
+
+interface CourseWithDetails extends Course {
+  category: Category;
+  subCategory: SubCategory;
+  level: Level;
+  sections: Section[];
+}
 
 const SearchPage = async ({ searchParams }: { searchParams: { query: string }}) => {
   const queryText = searchParams.query || ''
@@ -25,13 +33,13 @@ const SearchPage = async ({ searchParams }: { searchParams: { query: string }}) 
     orderBy: {
       createdAt: 'desc'
     }
-  });
+  }) as CourseWithDetails[];
 
   return (
     <div className="px-4 py-6 md:px-10 xl:px-16">
       <p className="text-lg md:text-2xl font-semibold mb-10">Recommended courses for {queryText}</p>
       <div className="flex gap-4 flex-wrap">
-        {courses.map((course) => (
+        {courses.map((course: CourseWithDetails) => (
           <CourseCard key={course.id} course={course} />
         ))}
       </div>
